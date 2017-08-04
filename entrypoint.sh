3fcf0764a8fd2c2859936e61d2140fbbf8313401
Fritz <fritz@Fritzs-MacBook-Pro.local>
2017-08-03 22:00:35 -0400

    inactive is not a valid keyword updated to disconnected

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,7 +2,7 @@
 while :
 do
     echo "Checking for inactive hosts every $INTERVAL seconds..."
-    rancher hosts -a | grep -w inactive | while read LINE
+    rancher hosts -a | grep -w disconnected | while read LINE
     do
         HOST=$(echo $LINE | cut -d ' ' -f 1)
         echo "Delete $LINE"
