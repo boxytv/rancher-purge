ee6702067c00cc527b35b91ef2c28ec74fe15572
Fritz <fritz@Fritzs-MacBook-Pro.local>
2017-08-03 22:24:35 -0400

    Updated entrypoint to deactivate first

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,6 +5,8 @@ do
     rancher hosts -a | grep -w disconnected | while read LINE
     do
         HOST=$(echo $LINE | cut -d ' ' -f 1)
+        echo "Deactivate $LINE"
+        rancher deactivate --type host $HOST
         echo "Delete $LINE"
         rancher rm --type host $HOST
     done
