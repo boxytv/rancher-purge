4a1e6124670919b8e4ab1029aa33d33f395e15c3
Fritz <fritz@Fritzs-MacBook-Pro.local>
2017-08-12 22:48:17 -0400

    Switched back to inactive keyword

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,11 +2,8 @@
 while :
 do
     echo "Checking for inactive hosts every $INTERVAL seconds..."
-    rancher hosts -a | grep -w disconnected | while read LINE
+    rancher hosts -a | grep -w inactive | while read LINE
     do
-        HOST=$(echo $LINE | cut -d ' ' -f 1)
-        echo "Deactivate $LINE"
-        rancher deactivate --type host $HOST
         echo "Delete $LINE"
         rancher rm --type host $HOST
     done
