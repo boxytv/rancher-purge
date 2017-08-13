39448e1ea5743ebc2a584729d1c3479ee8f93d93
Fritz <fritz@Fritzs-MacBook-Pro.local>
2017-08-12 23:22:36 -0400

    clean up syntax error

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,6 +4,7 @@ do
     echo "Checking for inactive hosts every $INTERVAL seconds..."
     rancher hosts -a | grep -w inactive | while read LINE
     do
+        HOST=$(echo $LINE | cut -d ' ' -f 1)
         echo "Delete $LINE"
         rancher rm --type host $HOST
     done
